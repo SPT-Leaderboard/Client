681ac4307ef773acde541541074f5542b7ebf587
Katrin0522 <katrinfox0522@gmail.com>
2025-10-18 19:05:36 +0300

    Invrease ver for client mod release

--- client/Data/GlobalData.cs
+++ client/Data/GlobalData.cs
@@ -6,7 +6,7 @@ namespace SPTLeaderboard.Data;
 
 public static class GlobalData
 {
-    public const string Version = "4.1.1";
+    public const string Version = "4.1.2";
     
 #if DEBUG || BETA
     public const string SubVersion = "4";
