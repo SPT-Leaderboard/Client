69188c5edbd52eacdbbfdb7b180c2400f0bd6f24
Kat <katrinfox0522@gmail.com>
2025-12-10 15:15:22 +0300

    Short SPTLB label in watermark

--- client/Patches/LeaderboardVersionLabelPatch.cs
+++ client/Patches/LeaderboardVersionLabelPatch.cs
@@ -30,11 +30,11 @@ namespace SPTLeaderboard.Patches
 			if (!string.IsNullOrEmpty(string_4))
 				str = str + " | " + string_4;
 #if DEBUG
-			str = str + " | " + $"SPT Leaderboard {GlobalData.Version} [DEBUG] - {GlobalData.SubVersion}";
+			str = str + " | " + $"SPTLB {GlobalData.Version} [DEBUG] - {GlobalData.SubVersion}";
 #elif BETA
-			str = str + " | " + $"SPT Leaderboard {GlobalData.Version} [BETA] - {GlobalData.SubVersion}";
+			str = str + " | " + $"SPTLB {GlobalData.Version} [BETA] - {GlobalData.SubVersion}";
 #else
-			str = str + " | " + $"SPT Leaderboard {GlobalData.Version}";
+			str = str + " | " + $"SPTLB {GlobalData.Version}";
 #endif
 			
 			var labelField = AccessTools.Field(typeof(PreloaderUI), "_alphaVersionLabel");
