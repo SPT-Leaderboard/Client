8ac3a79053794720bd97435dc864a6aae5955251
Katrin0522 <egorsimonoff2018@gmail.com>
2026-05-05 00:09:58 +0300

    Temp: Fix label patch

--- client/Patches/LeaderboardVersionLabelPatch.cs
+++ client/Patches/LeaderboardVersionLabelPatch.cs
@@ -12,38 +12,55 @@ namespace SPTLeaderboard.Patches
 		{
 			return typeof(PreloaderUI).GetMethod("method_6");
 		}
-
+		
 		[PatchPrefix]
-		static bool Prefix(PreloaderUI __instance)
+		public static void Prefix(ref string ___string_5, ref LocalizedText ____alphaVersionLabel)
 		{
-			string string_2 = (string)AccessTools.Field(typeof(PreloaderUI), "string_2").GetValue(__instance);
-			string string_3 = (string)AccessTools.Field(typeof(PreloaderUI), "string_3").GetValue(__instance);
-			string string_4 = (string)AccessTools.Field(typeof(PreloaderUI), "string_4").GetValue(__instance);
-			string string_5 = (string)AccessTools.Field(typeof(PreloaderUI), "string_5").GetValue(__instance);
-
-			string str = string_2;
-
-			if (!string.IsNullOrEmpty(string_3))
-				str = str + " | " + string_3;
-			if (!string.IsNullOrEmpty(string_5))
-				str = str + " | " + string_5;
-			if (!string.IsNullOrEmpty(string_4))
-				str = str + " | " + string_4;
+			bool flag = !___string_5.Contains("SPTLB");
+			if (flag)
+			{
 #if DEBUG
-			str = str + " | " + $"SPTLB {GlobalData.Version} [DEBUG] - {GlobalData.SubVersion}";
+				___string_5 += " | " + $"SPTLB {GlobalData.Version} [DEBUG] - {GlobalData.SubVersion}";
 #elif BETA
-			str = str + " | " + $"SPTLB {GlobalData.Version} [BETA] - {GlobalData.SubVersion}";
+				___string_5 += " | " + $"SPTLB {GlobalData.Version} [BETA] - {GlobalData.SubVersion}";
 #else
-			str = str + " | " + $"SPTLB {GlobalData.Version}";
+				___string_5 += " | " + $"SPTLB {GlobalData.Version}";
 #endif
-			
-			var labelField = AccessTools.Field(typeof(PreloaderUI), "_alphaVersionLabel");
-			var label = labelField.GetValue(__instance);
-
-			var locKeyProperty = label.GetType().GetProperty("LocalizationKey");
-			locKeyProperty?.SetValue(label, str);
-
-			return false;
+				____alphaVersionLabel.LocalizationKey = ___string_5;
+			}
 		}
+
+// 		[PatchPrefix]
+// 		static bool Prefix(PreloaderUI __instance)
+// 		{
+// 			string string_2 = (string)AccessTools.Field(typeof(PreloaderUI), "string_2").GetValue(__instance);
+// 			string string_3 = (string)AccessTools.Field(typeof(PreloaderUI), "string_3").GetValue(__instance);
+// 			string string_4 = (string)AccessTools.Field(typeof(PreloaderUI), "string_4").GetValue(__instance);
+// 			string string_5 = (string)AccessTools.Field(typeof(PreloaderUI), "string_5").GetValue(__instance);
+//
+// 			string str = string_2;
+//
+// 			if (!string.IsNullOrEmpty(string_3))
+// 				str = str + " | " + string_3;
+// 			if (!string.IsNullOrEmpty(string_5))
+// 				str = str + " | " + string_5;
+// 			if (!string.IsNullOrEmpty(string_4))
+// 				str = str + " | " + string_4;
+// #if DEBUG
+// 			str = str + " | " + $"SPTLB {GlobalData.Version} [DEBUG] - {GlobalData.SubVersion}";
+// #elif BETA
+// 			str = str + " | " + $"SPTLB {GlobalData.Version} [BETA] - {GlobalData.SubVersion}";
+// #else
+// 			str = str + " | " + $"SPTLB {GlobalData.Version}";
+// #endif
+// 			
+// 			var labelField = AccessTools.Field(typeof(PreloaderUI), "_alphaVersionLabel");
+// 			var label = labelField.GetValue(__instance);
+//
+// 			var locKeyProperty = label.GetType().GetProperty("LocalizationKey");
+// 			locKeyProperty?.SetValue(label, str);
+//
+// 			return false;
+// 		}
 	}
 }
