1bd3162f12a876f8ffcc65fe770f9ede95b7d6ea
Katrin0522 <katborowsky@gmail.com>
2026-09-15 01:08:23 +0300

    pump ver

--- src/Client/LeaderboardPlugin.cs
+++ src/Client/LeaderboardPlugin.cs
@@ -18,7 +18,7 @@ using UnityEngine;
 
 namespace SPTLeaderboard
 {
-    [BepInPlugin("harmonyzt.SPTLeaderboard", "SPTLeaderboard.Client", "5.1.9")]
+    [BepInPlugin("harmonyzt.SPTLeaderboard", "SPTLeaderboard.Client", "5.1.1")]
     public class LeaderboardPlugin : BaseUnityPlugin
     {
         public static LeaderboardPlugin Instance { get; private set; }
--- src/Client/Properties/AssemblyInfo.cs
+++ src/Client/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("5.1.0")]
-[assembly: AssemblyFileVersion("5.1.0")]
+[assembly: AssemblyVersion("5.1.1")]
+[assembly: AssemblyFileVersion("5.1.1")]
 
 [assembly: AssemblyTitle("SPT.Leaderboard")]
 [assembly: AssemblyProduct("SPT.Leaderboard")]
 [assembly: AssemblyDescription("Track your progress, compete with friends and connect with other SPT players worldwide through interactive website!")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("SPT.Leaderboard")]
-[assembly: AssemblyCopyright("Copyright© 2025")]
+[assembly: AssemblyCopyright("Copyright© 2026")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8fd112e1-0467-4938-b63b-305e03e44069")]
